9ed3e262c5769399b6b791a95542e6fe30f128a6
Kenneth Giusti <kgiusti@redhat.com>
2019-04-12 10:14:56 -0400

    use ipv4 loopback address since localhost may not be configured

--- docker/2-router/tests/test_smoke_01.sh
+++ docker/2-router/tests/test_smoke_01.sh
@@ -3,41 +3,41 @@
 
 set -x
 
-ombt2 --url amqp://localhost:15672 rpc-server --daemon
-ombt2 --url amqp://localhost:15672 rpc-server --daemon
+ombt2 --url amqp://127.0.0.1:15672 rpc-server --daemon
+ombt2 --url amqp://127.0.0.1:15672 rpc-server --daemon
 
-ombt2 --url amqp://localhost:25672 rpc-client --daemon
-ombt2 --url amqp://localhost:25672 rpc-client --daemon
-ombt2 --url amqp://localhost:25672 rpc-client --daemon
-ombt2 --url amqp://localhost:25672 rpc-client --daemon
+ombt2 --url amqp://127.0.0.1:25672 rpc-client --daemon
+ombt2 --url amqp://127.0.0.1:25672 rpc-client --daemon
+ombt2 --url amqp://127.0.0.1:25672 rpc-client --daemon
+ombt2 --url amqp://127.0.0.1:25672 rpc-client --daemon
 
-ombt2 --url amqp://localhost:25672 listener --daemon
-ombt2 --url amqp://localhost:25672 listener --daemon
+ombt2 --url amqp://127.0.0.1:25672 listener --daemon
+ombt2 --url amqp://127.0.0.1:25672 listener --daemon
 
-ombt2 --url amqp://localhost:15672 notifier --daemon
-ombt2 --url amqp://localhost:15672 notifier --daemon
-ombt2 --url amqp://localhost:15672 notifier --daemon
+ombt2 --url amqp://127.0.0.1:15672 notifier --daemon
+ombt2 --url amqp://127.0.0.1:15672 notifier --daemon
+ombt2 --url amqp://127.0.0.1:15672 notifier --daemon
 
 sleep 1
 
 
 for (( len=1024 ; len<=65536 ; len=len+1023 )) ; do
 
-    ombt2 --url amqp://localhost:15672 controller notify --events 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:15672 controller rpc-call --calls 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:15672 controller rpc-cast --calls 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:15672 controller rpc-fanout --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:15672 controller notify --events 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:15672 controller rpc-call --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:15672 controller rpc-cast --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:15672 controller rpc-fanout --calls 100 --pause 0.10 --length $len
 
-    ombt2 --url amqp://localhost:25672 controller notify --events 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:25672 controller rpc-call --calls 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:25672 controller rpc-cast --calls 100 --pause 0.10 --length $len
-    ombt2 --url amqp://localhost:25672 controller rpc-fanout --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:25672 controller notify --events 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:25672 controller rpc-call --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:25672 controller rpc-cast --calls 100 --pause 0.10 --length $len
+    ombt2 --url amqp://127.0.0.1:25672 controller rpc-fanout --calls 100 --pause 0.10 --length $len
 
     echo "SLEEP..."
     sleep 2
 done
 
 
-ombt2 --url amqp://localhost:15672 controller shutdown
+ombt2 --url amqp://127.0.0.1:15672 controller shutdown
 set +x
 
